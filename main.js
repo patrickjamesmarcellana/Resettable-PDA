8a541a767737cbb53f0a4ccb706c7355abd05082
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-08-05 19:08:06 +0800

    Complete storing machine into js variables

--- main.js
+++ main.js
@@ -3,31 +3,34 @@ const fs = require('fs')    // file-system
 let input_machine = []      // array of string input lines from text file
 let states = []             // array of state objects 
 let input_end_marker = '' 
+let current_line_index = 0
+
+let stack = []
 
 class State {
     constructor(name) {
         this.name = name
-        this.isInitial = false // default
-        this.isFinal = false   // default
-        this.isReset = false   // default
+        this.is_initial = false // default
+        this.is_final = false   // default
+        this.is_reset = false   // default
         this.transitions = []
     }
 
-    addTransition = (trans_arr) => {
+    add_transition = (trans_arr) => {
         const new_transition = new Transition(trans_arr[0], trans_arr[1], trans_arr[2], trans_arr[3], trans_arr[4])
         this.transitions.push(new_transition)
     }
 
-    setAsInitial = () => {
-        this.isInitial = true
+    set_as_initial = () => {
+        this.is_initial = true
     }
 
-    setAsFinal = () => {
-        this.isFinal = true
+    set_as_final = () => {
+        this.is_final = true
     }
 
-    setAsReset = () => {
-        this.isReset = true
+    set_as_reset = () => {
+        this.is_reset = true
     }
 }
 
@@ -41,7 +44,7 @@ class Transition {
     }
 }
 
-function readFile(filename) {
+function read_file(filename) {
     input_machine = fs.readFileSync(filename).toString('UTF8').split("\r\n")
 }
 
@@ -75,26 +78,53 @@ function extract_transitions(input_machine) {
 
         // find state from the array of states
         const state_index = search_state(state_name)
-        states[state_index].addTransition(trans_arr) // add transition to state object's list of transitions
-        console.log(states[state_index])
+        states[state_index].add_transition(trans_arr) // add transition to state object's list of transitions
     }
+
+    current_line_index = 5 + transition_count // to be used in referencing the next components in input_machine
+}
+
+function set_initial(input_machine) {
+    const initial_state_name = input_machine[current_line_index]
+    const state_index = search_state(initial_state_name)
+    states[state_index].set_as_initial()
+    current_line_index += 1
+}
+
+function set_initial_stack_symbol(input_machine) {
+    const stack_symbol = input_machine[current_line_index]
+    stack.push(stack_symbol)
+    current_line_index += 1
+}
+
+function set_final(input_machine) {
+    const final_state_names = input_machine[current_line_index].split(",")
+    for(state_name of final_state_names) {
+        const state_index = search_state(state_name)
+        states[state_index].set_as_final()
+    }
+    current_line_index += 1
+}
+
+function set_reset(input_machine) {
+    const reset_state_names = input_machine[current_line_index].split(",")
+    for(state_name of reset_state_names) {
+        const state_index = search_state(state_name)
+        states[state_index].set_as_reset()
+    }
+    current_line_index += 1
 }
 
 
-
-
-
-
-
-
-
-
-
-readFile('sample_machine.txt')
+read_file('sample_machine.txt')
 extract_states(input_machine)
 extract_end_marker(input_machine)
 extract_transitions(input_machine)
-console.log(states[5].isFinal)
+set_initial(input_machine)
+set_initial_stack_symbol(input_machine)
+set_final(input_machine)
+set_reset(input_machine)
 
+console.log(states)
 
 
