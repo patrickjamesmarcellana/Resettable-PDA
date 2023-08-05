028a6ecf00af6affbab388eb6fc425e4c46bbe7d
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-08-05 18:17:46 +0800

    Added state objects

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fs = require('fs') // file-system
 
-let input_machine = []
+let input_machine = []   // array of string input lines from text file
+let states = []          // array of state objects 
+
 class State {
     constructor(name) {
         this.name = name
@@ -10,23 +12,48 @@ class State {
         this.transitions = []
     }
 
-    addTransition = () => {
-        this.transitions.push()
+    addTransition = (transition_string) => {
+        const trans_arr = transition_string.split(",")
+        const new_transition = new Transition(trans_arr[0], trans_arr[1], trans_arr[2], trans_arr[3], trans_arr[4])
+        this.transitions.push(new_transition)
     }
 
+    setAsInitial = () => {
+        this.isInitial = true
+    }
+
+    setAsFinal = () => {
+        this.isFinal = true
+    }
+
+    setAsReset = () => {
+        this.isReset = true
+    }
 }
 
 class Transition {
-    
+    constructor(curr_state, input, pop_symbol, next_state, push_symbol) {
+        this.curr_state = curr_state
+        this.input = input
+        this.pop_symbol = pop_symbol
+        this.next_state = next_state
+        this.push_symbol = push_symbol
+    }
 }
-
 
 function readFile(filename) {
     input_machine = fs.readFileSync(filename).toString('UTF8').split("\r\n")
 }
+
 function extract_components(input_machine) {
-    states = []
+    // create state objects
+    states_string = input_machine[0].split(",")
+    for(let state of states_string) {
+        const new_state = new State(state)
+        states.push(new_state)
+    }
 }
+
 
 
 
@@ -42,6 +69,6 @@ readFile('sample_machine.txt')
 console.log(input_machine)
 
 extract_components(input_machine)
-
+console.log(states)
 
 
